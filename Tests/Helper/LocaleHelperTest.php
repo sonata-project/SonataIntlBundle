6a980391f5acbd231d8a2cc6dfbd793d8dec0c71
Thomas Rabaix <thomas.rabaix@ekino.com>
2011-11-13 21:26:39 +0100

    [test] Isolate test

--- Tests/Helper/LocaleHelperTest.php
+++ Tests/Helper/LocaleHelperTest.php
@@ -18,31 +18,45 @@ use Symfony\Component\Locale\Locale;
 
 class LocaleHelperTest extends \PHPUnit_Framework_TestCase
 {
-    public function testLocale()
+    public function getHelper()
     {
         $localeDetector = $this->getMock('Sonata\IntlBundle\Locale\LocaleDetectorInterface');
         $localeDetector->expects($this->any())
             ->method('getLocale')->will($this->returnValue('fr'));
 
         $helper = new LocaleHelper('UTF-8', $localeDetector);
-
+        
+        return $helper;
+    }
+    
+    public function testLanguage()
+    {
+        $helper = $this->getHelper();
         $this->assertEquals('français', $helper->language('fr'));
+        $this->assertEquals('French', $helper->language('fr', 'en'));
+        //        $this->assertEquals('', $helper->language('fr', 'fake'));
+        
+    }
+    
+    public function testCountry()
+    {
+        $helper = $this->getHelper();
         $this->assertEquals('France', $helper->country('FR'));
+        $this->assertEquals('France', $helper->country('FR', 'en'));              
+//        $this->assertEquals('', $helper->country('FR', 'fake'));
+    }
+    
+    public function testLocale()
+    {
+        $helper = $this->getHelper();
+
         $this->assertEquals('français', $helper->locale('fr'));
         $this->assertEquals('français (Canada)', $helper->locale('fr_CA'));
 
-        $this->assertEquals('French', $helper->language('fr', 'en'));
-        $this->assertEquals('France', $helper->country('FR', 'en'));
         $this->assertEquals('French', $helper->locale('fr', 'en'));
         $this->assertEquals('French (Canada)', $helper->locale('fr_CA', 'en'));
-
-        // for now these tests are commented as the RessourceBundle always fallback to the
-        // system value ... don't know how the framework should behave.
-
-//        $this->assertEquals('', $helper->language('fr', 'fake'));
-//        $this->assertEquals('', $helper->country('FR', 'fake'));
-//        $this->assertEquals('', $helper->locale('fr', 'fake'));
-//        $this->assertEquals('', $helper->locale('fr_CA', 'fake'));
+        //        $this->assertEquals('', $helper->locale('fr', 'fake'));
+        //        $this->assertEquals('', $helper->locale('fr_CA', 'fake'));
 
     }
 }
