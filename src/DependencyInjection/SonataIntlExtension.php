87ceed96ab1e6e8a241bba2e05f45c7bab8a2dbd
Fran Moreno <franmomu@gmail.com>
2019-12-04 10:24:20 +0100

    Fix PHPStorm findings

--- src/DependencyInjection/SonataIntlExtension.php
+++ src/DependencyInjection/SonataIntlExtension.php
@@ -107,12 +107,15 @@ class SonataIntlExtension extends Extension
      */
     private function validateTimezones(array $timezones)
     {
-        try {
-            foreach ($timezones as $timezone) {
-                $tz = new \DateTimeZone($timezone);
+        foreach ($timezones as $timezone) {
+            try {
+                new \DateTimeZone($timezone);
+            } catch (\Exception $e) {
+                throw new \RuntimeException(sprintf(
+                    'Unknown timezone "%s". Please check your sonata_intl configuration.',
+                    $timezone
+                ));
             }
-        } catch (\Exception $e) {
-            throw new \RuntimeException(sprintf('Unknown timezone "%s". Please check your sonata_intl configuration.', $timezone));
         }
     }
 }
