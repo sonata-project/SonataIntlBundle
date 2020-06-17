22b156294631b6f8fbf9dc9380cf3bdf94f4134c
Javier Spagnoletti <phansys@gmail.com>
2020-06-17 12:27:40 -0300

    Allow "symfony/intl:^5"

--- src/Templating/Helper/LocaleHelper.php
+++ src/Templating/Helper/LocaleHelper.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 namespace Sonata\IntlBundle\Templating\Helper;
 
 use Symfony\Component\Intl\Countries;
-use Symfony\Component\Intl\Intl;
 use Symfony\Component\Intl\Languages;
 use Symfony\Component\Intl\Locales;
 
@@ -33,13 +32,6 @@ class LocaleHelper extends BaseHelper
      */
     public function country($code, $locale = null)
     {
-        // NEXT_MAJOR: Remove this when dropping < 4.3 Symfony support
-        if (!class_exists(Countries::class)) {
-            $name = Intl::getRegionBundle()->getCountryName($code, $locale ?: $this->localeDetector->getLocale());
-
-            return $name ? $this->fixCharset($name) : '';
-        }
-
         return $this->fixCharset(Countries::getName($code, $locale ?: $this->localeDetector->getLocale()));
     }
 
@@ -51,19 +43,6 @@ class LocaleHelper extends BaseHelper
      */
     public function language($code, $locale = null)
     {
-        // NEXT_MAJOR: Remove this when dropping < 4.3 Symfony support
-        if (!class_exists(Languages::class)) {
-            $codes = explode('_', $code);
-
-            $name = Intl::getLanguageBundle()->getLanguageName(
-                $codes[0],
-                $codes[1] ?? null,
-                $locale ?: $this->localeDetector->getLocale()
-            );
-
-            return $name ? $this->fixCharset($name) : '';
-        }
-
         return $this->fixCharset(Languages::getName($code, $locale ?: $this->localeDetector->getLocale()));
     }
 
@@ -75,13 +54,6 @@ class LocaleHelper extends BaseHelper
      */
     public function locale($code, $locale = null)
     {
-        // NEXT_MAJOR: Remove this when dropping < 4.3 Symfony support
-        if (!class_exists(Locales::class)) {
-            $name = Intl::getLocaleBundle()->getLocaleName($code, $locale ?: $this->localeDetector->getLocale());
-
-            return $name ? $this->fixCharset($name) : '';
-        }
-
         return $this->fixCharset(Locales::getName($code, $locale ?: $this->localeDetector->getLocale()));
     }
 
